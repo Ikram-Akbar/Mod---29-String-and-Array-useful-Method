a285b980aaa3ca291ee35b6032a81650cb2d7a6c
Ikram-Git-Coder <ikramakbar71@gmail.com>
2022-09-06 00:09:52 +0600

    toLowercase method

--- string.js
+++ string.js
@@ -1,3 +1,15 @@
-console.log(
-    "hii"
-);
+// String Comparison toLowerCase() and upperCase()
+
+// First method 
+const userInput = "I have an Idea for our programming ";
+console.log(userInput.toLocaleLowerCase());
+// second method 
+const stringConverter = (string) => {
+    return (string.toLocaleLowerCase())
+};
+// third method 
+const newString = stringConverter("I LOVE MY COUNTRY ");
+console.log(newString);
+// this is shortest method by using arrow function 
+const strConverter = str =>  (str.toLocaleLowerCase());
+console.log(strConverter("THIS IS ANOTER LINE "));
